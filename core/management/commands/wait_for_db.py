6ef27a47edaa1de74c059def3d20efe7dd81b890
duendereus <cespedesfernando967@gmail.com>
2024-05-22 19:23:03 -0600

    Building a Financial Credit Core with Django - Week 1: Initial Configurations. Docker, DB, GitHub actions, and Django configs

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available.
 """
 
 import time
+import os
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -15,12 +16,22 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
+        db_name = os.environ.get("DB_NAME")
+        db_user = os.environ.get("DB_USER")
+        db_host = os.environ.get("DB_HOST", "localhost")
+        db_port = os.environ.get("DB_PORT", "5432")
+        self.stdout.write(
+            f"Trying to connect to DB {db_name} on {db_host}:{db_port} as {db_user}"
+        )
+
         while db_up is False:
             try:
                 self.check(databases=["default"])
                 db_up = True
-            except (Psycopg2OpError, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+            except (Psycopg2OpError, OperationalError) as e:
+                self.stdout.write(
+                    f"Database unavailable, waiting 1 second... Error: {e}"
+                )
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
