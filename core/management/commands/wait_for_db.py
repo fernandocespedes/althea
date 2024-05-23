70aff596f5d591c944b05f2fa5e011f2c61daae9
duendereus <cespedesfernando967@gmail.com>
2024-05-22 19:55:27 -0600

    Minor updates week 1 (wait_for_db and checks.yml)

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -3,7 +3,6 @@ Django command to wait for the database to be available.
 """
 
 import time
-import os
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -16,13 +15,6 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
-        db_name = os.environ.get("DB_NAME")
-        db_user = os.environ.get("DB_USER")
-        db_host = os.environ.get("DB_HOST", "localhost")
-        db_port = os.environ.get("DB_PORT", "5432")
-        self.stdout.write(
-            f"Trying to connect to DB {db_name} on {db_host}:{db_port} as {db_user}"
-        )
 
         while db_up is False:
             try:
